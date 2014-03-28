2ffa24c7cdb8c3578db921831abc2478a700605b
Melanie Archer <job@twobanjos.com>
2014-03-28 10:32:12 -0700

    Format RSpec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@ require 'capybara/rspec'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
+  config.color_enabled = true
+  config.tty = true
+  config.formatter = :documentation # :progress, :html, :textmate
 end
 
 def app
