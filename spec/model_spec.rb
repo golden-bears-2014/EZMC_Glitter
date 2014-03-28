dec98f8b90b403e3815a7b115b1cb3e4e0659720
Apprentice <apprentice@dbc15.local>
2014-03-28 13:25:37 -0700

    added shoulda testers for models and assocations

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -9,7 +9,27 @@ describe 'model' do
       it {should allow_value('My Survey').for(:title)}
   end
 
+  describe User do
+    it {should have_many(:surveys)}
+    it {should have_many(:responses)}
+    it {should have_many(:options).through(:responses)}
+    it {should_not allow_value("").for(:email)}
+    it {should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:password_hash)}
+  end
 
+  describe Response do
+    it {should belong_to(:user)}
+    it {should belong_to(:option)}
+  end
+
+  describe Option do
+    it {should have_many(:responses)}
+    it {should have_many(:users).through(:responses)}
+    it {should allow_value("This is an option").for(:text)}
+  end
 
 
 end
